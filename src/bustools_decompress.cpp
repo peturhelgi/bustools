bf32c1d22d5ba7f401e4cd143cbcfa8bb18cd0a9
Petur Helgi Einarsson <peturhelgi@gmail.com>
2022-07-10 16:00:01 +0000

    Add Fibonacci decoding.

--- src/bustools_decompress.cpp
+++ src/bustools_decompress.cpp
@@ -13,6 +13,61 @@
 #include "bustools_decompress.h"
 
 /**
+ * @brief Decode a single fibonacci number from a buffer
+ * @pre buf contains at least n_buf elements.
+ * @pre 0 <= i_fibo < fibo64.size()
+ * @pre 0 <= bitpos < 64
+ * @pre 0 <= bufpos < n_buf
+ *
+ * @param buf The buf to fibo-decode from.
+ * @param n_buf maximum number of elements in the buffer.
+ * @param i_fibo a counter of previously seen fibonacci numbers.
+ * @param bitpos a bit offset from the left within buf[bufpos].
+ * @param bufpos offset from the beginning of buf.
+ * 
+ * @return uint64_t num; the fibonacci decoded number. If buf is exhausted before the termination bit of
+ * a fibonacci number, num represents a partially decoded number.
+ */
+uint64_t fiboDecodeSingle(uint64_t const *const buf, const size_t n_buf, uint32_t &i_fibo, uint32_t &bitpos, uint32_t &bufpos)
+{
+
+	size_t SIZE = sizeof(uint64_t) * 8;
+	size_t buf_offset = bufpos;
+	size_t bit_offset = SIZE - (bitpos % SIZE) - 1;
+	uint64_t num{0};
+
+	int bit = (buf[buf_offset] >> bit_offset) & 1;
+	int last_bit = 0;
+
+	++bitpos;
+	bool print =0;
+	bool print_fibo = print && 0;
+	bool print_bits = print && !print_fibo;
+
+	while (last_bit + bit < 2 && buf_offset < n_buf)
+	{
+		last_bit = bit;
+		num += bit * (fibo64[i_fibo]);
+
+		buf_offset = bufpos + bitpos / SIZE;
+		bit_offset = SIZE - (bitpos % SIZE) - 1;
+
+		bit = (buf[buf_offset] >> bit_offset) & 1;
+
+		++bitpos;
+		++i_fibo;
+	}
+	if (last_bit + bit == 2)
+	{
+		i_fibo = 0;
+	}
+
+	bufpos += (bitpos / SIZE);
+	bitpos %= SIZE;
+	return num;
+}
+
+/**
  * @brief Decompress barcodes using fibonacci-runlength(0)-delta decoding.
  * 
  * @param BUF The char array occupied by at least `row_count` encoded numbers, encoded using delta-runlenght(0)-fibonacci.
